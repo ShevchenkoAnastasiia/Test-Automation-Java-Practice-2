0a910881f7d2920580e3ff87c32c1848f3be2583
ShevchenkoAnastasiia <93194293+ShevchenkoAnastasiia@users.noreply.github.com>
2021-10-27 17:17:22 +0300

    Add files via upload

---
+++ src_main_java_com_epam_test_automation_java_practice2_Main.java
@@ -0,0 +1,81 @@
+package com.epam.test.automation.java.practice2;
+
+public class Main {
+
+    /**
+     * <summary>
+     * Implement code according to description of task 1.
+     * </summary>
+     */
+    public static int task1(int value) {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task 2.
+     * </summary>
+     */
+    public static int task2(int N) {
+         static int countBinaries(int N)
+    {
+        int ctr = 1;
+        int ans = 0;
+        while (N > 0)
+        {
+
+            if (N % 10 == 1)
+            {
+                ans += Math.pow(2, ctr - 1);
+            }
+
+            else if (N % 10 > 1)
+            {
+                ans = (int) (Math.pow(2, ctr) - 1);
+            }
+            ctr++;
+            N /= 10;
+        }
+        return ans;
+    }
+
+}
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task 3.
+     * </summary>
+     */
+    public static int calculateSum(int n) {
+
+            if (n <= 0)
+                return 0;
+
+            int fibo[]=new int[n+1];
+            fibo[0] = 0; fibo[1] = 1;
+
+            int sum = fibo[0] + fibo[1];
+
+            for (int i=2; i<=n; i++)
+            {
+                fibo[i] = fibo[i-1]+fibo[i-2];
+                sum += fibo[i];
+            }
+
+            return sum;
+        }
+
+        public static void main(String args[])
+        {
+            int n = 4;
+            System.out.println("Sum of Fibonacci" +
+                    " numbers is : "+ calculateSum(n));
+        }
+
+
+        throw new UnsupportedOperationException();
+    }
+}
